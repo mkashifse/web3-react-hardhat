c41d7dcc1db8eb3b6bc8a5d813e5c1e83938793a
Muhammad Kashif <mk.kashif.se@gmail.com>
2022-06-05 14:01:52 -0700

    file moved

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import "./App.css";
+import { useEffect } from "react";
+import contractAbi  from "./../../hardhat/artifacts/contracts/Greeter.sol/Greeter.json";
 
 function App() {
+  useEffect(() => {
+    // const web3 = new Web3("http://127.0.0.1:8545/");
+    // const contract = web3.eth.Contract(contractAbi.abi, "0x5FbDB2315678afecb367f032d93F642f64180aa3");
+    // console.log(contract);
+  }, []);
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
