bfc5975bb218140da76b7f3bc8aa3c6bc4147dfe
Muhammad Kashif <mk.kashif.se@gmail.com>
2022-06-05 15:13:09 -0700

    mehtod wrapper added

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,63 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Web3 from "web3/dist/web3.min.js";
 import contractAbi from "./hardhat/artifacts/contracts/Greeter.sol/Greeter.json";
 
+const web3Init = (onConnect) => {
+  let defaultAccount = "";
+  const web3 = new Web3("http://127.0.0.1:8545/");
+  const connector = window.ethereum;
+  const contract = new web3.eth.Contract(
+    contractAbi.abi,
+    "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+  );
+
+  connector
+    .request({
+      method: "eth_requestAccounts",
+    })
+    .then(([account]) => {
+      defaultAccount = account;
+      onConnect({ defaultAccount, contract, web3 });
+    });
+};
+
+let contract, defaultAccount, web3;
+
+const getMethod = (name, params = null) => {
+  return new Promise((resolve, reject) => {
+    if (params) {
+    } else {
+      contract.methods[name]().call(
+        { from: defaultAccount },
+        (err, resp) => {
+          if (err) reject(err);
+          else resolve(resp);
+        }
+      );
+    }
+  });
+};
+
 function App() {
   useEffect(() => {
-    const web3 = new Web3("http://127.0.0.1:8545/");
-    const provider = window.ethereum;
-    // const defaultAccount = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-    const contract = new web3.eth.Contract(
-      contractAbi.abi,
-      "0x5fbdb2315678afecb367f032d93f642f64180aa3"
-    );
-    provider
-      .request({
-        method: "eth_requestAccounts",
-      })
-      .then(([account]) => {
-        console.log(account);
-        contract.methods
-          .greet()
-          .call({ from: account }, (err, resp) => {
-            console.log(resp);
-          });
-      });
+    web3Init((obj) => {
+      contract = obj.contract;
+      defaultAccount = obj.defaultAccount;
+      web3 = obj.web3;
+    });
   }, []);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const onClick = async () => {
+    const resp = await getMethod("greet");
+    console.log(resp);
+  };
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <button onClick={onClick}>Greet</button>
+    </h1>
+  );
 }
 
 export default App;
